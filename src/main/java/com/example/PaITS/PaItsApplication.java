93708f17e887eb15550b3b044abb84cee0dbdd5e
Rajeev Gupta <rajeev.382761@gmail.com>
2026-02-10 20:59:58 +0530

    init: spring boot base project

---
+++ src/main/java/com/example/PaITS/PaItsApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaITS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaItsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaItsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PaITS/PaItsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaITS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaItsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

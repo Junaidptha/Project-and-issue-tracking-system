1979c941e6bc5923858e0b700004bb0e33ba5daa
Junaidptha <khan619junaid@gmail.com>
2026-04-19 00:00:32 +0530

    Fix API base URL to work in both local and production environments

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -1,6 +1,8 @@
 // api.js
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = window.location.origin === 'http://localhost:8080' 
+    ? 'http://localhost:8080' 
+    : '';
 
 // Centralised config for tokens
 const tokenManager = {
